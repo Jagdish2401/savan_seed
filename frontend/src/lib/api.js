2326a87437e3e4ec97e05051f368d054b1e1d900
Jagdish2401 <jagdishhadiyal2401@gmail.com>
2026-08-23 16:23:51 +0530

    10

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -5,6 +5,7 @@ const baseURL = (import.meta.env.VITE_API_BASE_URL || '').replace(/\/+$/, '');
 export const api = axios.create({
   baseURL,
   withCredentials: true,
+  
 });
 
 api.interceptors.request.use((config) => {
